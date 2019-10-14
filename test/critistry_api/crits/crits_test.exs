c873547102370ccf74b2cdfcb19e9e23e7c4d587
Eric Hsieh <ehsiehweb@gmail.com>
2019-10-13 23:32:20 -0700

    Tests

--- test/critistry_api/crits/crits_test.exs
+++ test/critistry_api/crits/crits_test.exs
@@ -4,66 +4,66 @@ defmodule CritistryApi.CritsTest do
   alias CritistryApi.Crits
 
   describe "crit_requests" do
-    alias CritistryApi.Crits.CirtRequest
+    alias CritistryApi.Crits.CritRequest
 
     @valid_attrs %{description: "some description", image: "some image", title: "some title"}
     @update_attrs %{description: "some updated description", image: "some updated image", title: "some updated title"}
     @invalid_attrs %{description: nil, image: nil, title: nil}
 
-    def cirt_request_fixture(attrs \\ %{}) do
-      {:ok, cirt_request} =
+    def crit_request_fixture(attrs \\ %{}) do
+      {:ok, crit_request} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> Crits.create_cirt_request()
+        |> Crits.create_crit_request()
 
-      cirt_request
+      crit_request
     end
 
     test "list_crit_requests/0 returns all crit_requests" do
-      cirt_request = cirt_request_fixture()
-      assert Crits.list_crit_requests() == [cirt_request]
+      crit_request = crit_request_fixture()
+      assert Crits.list_crit_requests() == [crit_request]
     end
 
-    test "get_cirt_request!/1 returns the cirt_request with given id" do
-      cirt_request = cirt_request_fixture()
-      assert Crits.get_cirt_request!(cirt_request.id) == cirt_request
+    test "get_crit_request!/1 returns the crit_request with given id" do
+      crit_request = crit_request_fixture()
+      assert Crits.get_crit_request!(crit_request.id) == crit_request
     end
 
-    test "create_cirt_request/1 with valid data creates a cirt_request" do
-      assert {:ok, %CirtRequest{} = cirt_request} = Crits.create_cirt_request(@valid_attrs)
-      assert cirt_request.description == "some description"
-      assert cirt_request.image == "some image"
-      assert cirt_request.title == "some title"
+    test "create_crit_request/1 with valid data creates a crit_request" do
+      assert {:ok, %critRequest{} = crit_request} = Crits.create_crit_request(@valid_attrs)
+      assert crit_request.description == "some description"
+      assert crit_request.image == "some image"
+      assert crit_request.title == "some title"
     end
 
-    test "create_cirt_request/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Crits.create_cirt_request(@invalid_attrs)
+    test "create_crit_request/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Crits.create_crit_request(@invalid_attrs)
     end
 
-    test "update_cirt_request/2 with valid data updates the cirt_request" do
-      cirt_request = cirt_request_fixture()
-      assert {:ok, cirt_request} = Crits.update_cirt_request(cirt_request, @update_attrs)
-      assert %CirtRequest{} = cirt_request
-      assert cirt_request.description == "some updated description"
-      assert cirt_request.image == "some updated image"
-      assert cirt_request.title == "some updated title"
+    test "update_crit_request/2 with valid data updates the crit_request" do
+      crit_request = crit_request_fixture()
+      assert {:ok, crit_request} = Crits.update_crit_request(crit_request, @update_attrs)
+      assert %critRequest{} = crit_request
+      assert crit_request.description == "some updated description"
+      assert crit_request.image == "some updated image"
+      assert crit_request.title == "some updated title"
     end
 
-    test "update_cirt_request/2 with invalid data returns error changeset" do
-      cirt_request = cirt_request_fixture()
-      assert {:error, %Ecto.Changeset{}} = Crits.update_cirt_request(cirt_request, @invalid_attrs)
-      assert cirt_request == Crits.get_cirt_request!(cirt_request.id)
+    test "update_crit_request/2 with invalid data returns error changeset" do
+      crit_request = crit_request_fixture()
+      assert {:error, %Ecto.Changeset{}} = Crits.update_crit_request(crit_request, @invalid_attrs)
+      assert crit_request == Crits.get_crit_request!(crit_request.id)
     end
 
-    test "delete_cirt_request/1 deletes the cirt_request" do
-      cirt_request = cirt_request_fixture()
-      assert {:ok, %CirtRequest{}} = Crits.delete_cirt_request(cirt_request)
-      assert_raise Ecto.NoResultsError, fn -> Crits.get_cirt_request!(cirt_request.id) end
+    test "delete_crit_request/1 deletes the crit_request" do
+      crit_request = crit_request_fixture()
+      assert {:ok, %critRequest{}} = Crits.delete_crit_request(crit_request)
+      assert_raise Ecto.NoResultsError, fn -> Crits.get_crit_request!(crit_request.id) end
     end
 
-    test "change_cirt_request/1 returns a cirt_request changeset" do
-      cirt_request = cirt_request_fixture()
-      assert %Ecto.Changeset{} = Crits.change_cirt_request(cirt_request)
+    test "change_crit_request/1 returns a crit_request changeset" do
+      crit_request = crit_request_fixture()
+      assert %Ecto.Changeset{} = Crits.change_crit_request(crit_request)
     end
   end
 
