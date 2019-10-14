460d59419c654d495aa5db67c94a634d5e8a6ccf
Eric Hsieh <ehsiehweb@gmail.com>
2019-10-13 22:28:22 -0700

    Test Cloud Build Trigger

--- lib/critistry_api_web/resolvers/user.ex
+++ lib/critistry_api_web/resolvers/user.ex
@@ -2,7 +2,8 @@ defmodule CritistryApiWeb.Resolvers.User do
   def list_users(_parent, _args, _resolution) do
     {:ok, [
             %{username: "Eric"}, 
-            %{username: "John"}
+            %{username: "Colleen"},
+            %{username: "Allen"}
           ]
     }
   end
